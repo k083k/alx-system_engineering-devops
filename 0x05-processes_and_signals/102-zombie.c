b2d60d483506f290a401d4691c302208190bbeb7
k083k <kobekore@gmail.com>
2022-07-22 05:43:54 -0700

    Advanced Task

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,36 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-
-int infinite_while(void);
 
 /**
- * main - entry point, creates zombie processes
- *
- * Return: 0
- */
-int main(void)
-{
-	int i;
-	pid_t child_id;
-
-	for (i = 0; i < 5; i++)
-	{
-		child_id = fork();
-		if (child_id > 0)
-			printf("Zombie process created, PID: %ld\n", (long int) child_id);
-		else
-			exit(0);
-	}
-	infinite_while();
-	return (0);
-}
-
-/**
- * infinite_while - stalls program
- *
- * Return: 0
+ * infinite_while - creates an infinite loop to make the program hang
+ * Return: always 0
  */
 int infinite_while(void)
 {
@@ -38,5 +12,26 @@ int infinite_while(void)
 	{
 		sleep(1);
 	}
+	return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	pid_t zombie;
+
+	for (i = 0; i < 5; i++)
+	{
+		zombie = fork();
+		if (!zombie)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie);
+	}
+
+	infinite_while();
 	return (0);
 }
